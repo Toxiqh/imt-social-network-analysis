5a62422d5354d5b8cf68b06354595d84e048e3f0
dams <damien.carrier95@gail.com>
2022-02-15 11:46:09 +0100

    feat(footer/header) : Adding footer and header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <p>
+      Bienvenue sur notre application d'analyse de réseaux sociaux
+    </p>
+  </header>
+);
+
+export default Header;
