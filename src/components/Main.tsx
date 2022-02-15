122f0b9a6786b8bc3499d5297f528b07a56d70cd
dams <damien.carrier95@gail.com>
2022-02-15 15:32:10 +0100

    feat(Results) : Adding results components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Stack} from 'react-bootstrap';
+import Results from "./Results";
 
 const Main = () => (
-  <Stack gap={3} className="App-main">
+  <Stack className="App-main">
     <div className="Db-Selector"></div>
     <div className="Actions-Btn"></div>
-    <div className="Result-display"></div>
+    <div className="Result-display">
+      <Results/>
+    </div>
     <div className="Clear-Btn"></div>
   </Stack>
 );
