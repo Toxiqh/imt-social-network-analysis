ad8a1775fe5d93ac2c058ca88d55e5e5fca30621
Clément <clement.savinaud@mines-ales.org>
2022-02-15 17:11:51 +0100

    feature : database toggle button added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Stack} from 'react-bootstrap';
 import Results from "./Results";
+import Switch from "./Switch";
 
 const Main = () => (
   <Stack className="App-main">
-    <div className="Db-Selector"></div>
+    <div className="Db-Selector">
+        <Switch/>
+    </div>
     <div className="Actions-Btn"></div>
     <div className="Result-display">
       <Results/>
