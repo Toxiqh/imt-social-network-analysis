7b8732366d8dfb3f3168174693e2646473386452
Clément <clement.savinaud@mines-ales.org>
2022-02-19 16:39:21 +0100

    feature : ajout du container d'actions btn et des boutons associés
    ajout du boutton d'insertion de données
    ajout du boutton de selection de données

---
+++ src/components/InsertBtn.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import '../styles/ActionBtn.css'
+
+const InsertBtn = () => {
+    return (
+        <div className="Btn">Insertion de données</div>
+    )
+};
+
+export default InsertBtn;
