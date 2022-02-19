74cb654288c14feedabb5a0298225157c48c2eae
Clément <clement.savinaud@mines-ales.org>
2022-02-19 16:39:21 +0100

    feature : ajout du container d'actions btn et des boutons associés
    ajout du boutton d'insertion de données
    ajout du boutton de selection de données

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import {Stack} from 'react-bootstrap';
 import Results from "./Results";
 import Switch from "./Switch";
+import InsertBtn from "./InsertBtn";
+import SelectBtn from "./SelectBtn";
+import '../styles/Main.css'
 
 const Main = () => (
   <Stack className="App-main">
     <div className="Db-Selector">
         <Switch/>
     </div>
-    <div className="Actions-Btn"></div>
+    <div className="Actions-Btn-Container">
+        <div className="Action-Btn">
+            <InsertBtn/>
+        </div>
+        <div className="Action-Btn">
+            <SelectBtn/>
+        </div>
+    </div>
     <div className="Result-display">
       <Results/>
     </div>
---
+++ src/components/SelectBtn.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import '../styles/ActionBtn.css'
+
+const SelectBtn = () => {
+    return (
+        <div className="Btn">Récupérer des données</div>
+    )
+};
+
+export default SelectBtn;
